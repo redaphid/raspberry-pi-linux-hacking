baf091c47937cae07799531b058dfdec235096fe
redaphid <iam@aaronherres.com>
2021-09-06 01:39:14 -0700

    forgot the step that actually updates the firmware

--- enable-usb-boot.sh
+++ enable-usb-boot.sh
@@ -4,4 +4,5 @@ vcgencmd bootloader_config > bootconf.txt
 sed -i -e '/^BOOT_ORDER=/ s/=.*$/=0xf41/' bootconf.txt
 rpi-eeprom-config --out pieeprom-new.bin --config bootconf.txt /lib/firmware/raspberrypi/bootloader/critical/pieeprom-2020-09-03.bin
 rpi-eeprom-config --out pieeprom-new.bin --config bootconf.txt /lib/firmware/raspberrypi/bootloader/critical/pieeprom-2020-09-03.bin
-reboot
+rpi-eeprom-update -d -f ./pieeprom-new.bin
+shutdown -h now
