5e2a188f82beffb6cd01ae234a859ccb09fe3a61
redaphid <iam@aaronherres.com>
2021-09-06 01:36:38 -0700

    missed critical step in firmware updating to use usb

--- enable-usb-boot.sh
+++ enable-usb-boot.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 apt install rpi-eeprom
 vcgencmd bootloader_config > bootconf.txt
+sed -i -e '/^BOOT_ORDER=/ s/=.*$/=0xf41/' bootconf.txt
 rpi-eeprom-config --out pieeprom-new.bin --config bootconf.txt /lib/firmware/raspberrypi/bootloader/critical/pieeprom-2020-09-03.bin
 rpi-eeprom-config --out pieeprom-new.bin --config bootconf.txt /lib/firmware/raspberrypi/bootloader/critical/pieeprom-2020-09-03.bin
 reboot
---
+++ setup-disk.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+git clone --depth=1 git://github.com/raspberrypi/firmware.git
